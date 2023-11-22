fe752704ffbb0d905beab863e03e48718d748f0e
Arjun Menon <arjun.slugger@gmail.com>
2023-11-21 19:45:35 -0500

    Add files via upload

--- playerrunpasstutorial.R
+++ playerrunpasstutorial.R
@@ -2,24 +2,26 @@ install.packages("nflreadr")
 install.packages("tidyverse")
 install.packages("gt")
 install.packages("gtExtras")
+install.packages("nflfastR")
 library(nflreadr)
 library(tidyverse)
 library(gt)
 library(gtExtras)
+library(nflfastR)
 
-pbp <- load_participation(2023, include_pbp = TRUE)
+### load in data
+pbp <- nflreadr::load_participation(2023, include_pbp = TRUE)
 
+### check column names
 names(pbp)
 
+### make players their own row
 pbp <- pbp |> 
   separate_rows(offense_players, sep = ";") |> 
-  select(old_game_id, play_id, posteam, defteam, week, offense_players, offense_personnel, offense_formation, 
+  select(old_game_id, play_id, posteam, defteam, week, offense_players, offense_personnel, offense_formation,
          rush, pass, wp, down, ydstogo, yardline_100, score_differential, pass_oe)
 
-rosters <- load_rosters(2023)
-
-pbp <- left_join(pbp, rosters |> select(gsis_id, full_name, position), by = c('offense_players' = 'gsis_id'))
-
+### filter to just offensive plays
 pbp <- pbp |> 
   filter(rush == 1 | pass == 1)
 
@@ -43,19 +45,24 @@ pbp <- pbp |>
   )
 
 pbp$`Down & Distance` <- factor(pbp$`Down & Distance`, levels = c('1st & 10', '1st & 11+',
-                                                                    '2nd & 1', '2nd & 2',
-                                                                    '2nd & 3-6', '2nd & 7-9', '2nd & 10+',
-                                                                    '3rd & 1', '3rd & 2', '3rd & 3-6',
-                                                                    '3rd & 7-9', '3rd & 10+',
-                                                                    '4th Down'))
+                                                                  '2nd & 1', '2nd & 2',
+                                                                  '2nd & 3-6', '2nd & 7-9', '2nd & 10+',
+                                                                  '3rd & 1', '3rd & 2', '3rd & 3-6',
+                                                                  '3rd & 7-9', '3rd & 10+',
+                                                                  '4th Down'))
 
+rosters <- load_rosters(2023)
+
+### get names for every player row
+pbp <- left_join(pbp, rosters |> select(gsis_id, full_name, position), by = c('offense_players' = 'gsis_id'))
 
 pbp |> 
   filter(wp >= 0.025 & wp <= 0.975, position %in% c('HB', 'WR', 'TE', 'FB')) |> 
   group_by(full_name) |> 
   summarise(
     passrate = mean(pass, na.rm = T),
-    plays = n()
+    plays = n(),
+    PROE = mean(pass_oe, na.rm = T)
   ) |> filter(passrate >= 0.8 | passrate <= 0.2, plays >= 50) |> 
   arrange(-passrate)
 
@@ -64,50 +71,45 @@ pbp |>
   group_by(full_name, down) |> 
   summarise(
     passrate = mean(pass, na.rm = T),
-    plays = n()
-  ) |> filter(passrate >= 0.8 | passrate <= 0.2, plays >= 20, down != 3) |> 
+    plays = n(),
+    PROE = mean(pass_oe, na.rm = T)
+  ) |> filter(passrate >= 0.8 | passrate <= 0.2, plays >= 20, down %in% c(1, 2)) |> 
   arrange(-passrate)
 
 pbp |> 
-  filter(posteam == 'BUF', wp >= 0.025 & wp <= 0.975, score_differential > 0) |> 
-  group_by(play_id) |> 
-  mutate(
-    Knoxin = any(full_name == 'Dawson Knox'),
-    Kincaidin = any(full_name == 'Dalton Kincaid')
-  ) |>  slice(1) |> 
-  group_by(Knoxin, Kincaidin) |> 
-  summarise(
-    plays = n(),
-    passrate = mean(pass, na.rm = T)
-  )
-  
-pbp |> 
-  filter(posteam == 'BUF', wp >= 0.025 & wp <= 0.975, down %in% c(1, 2)) |> 
+  filter(posteam == 'BUF', wp >= 0.025 & wp <= 0.975) |> 
   group_by(play_id) |> 
   mutate(
     Knoxin = any(full_name == 'Dawson Knox'),
     Kincaidin = any(full_name == 'Dalton Kincaid')
   ) |> slice(1) |> 
-  group_by(Knoxin, Kincaidin) |>  
+  group_by(Knoxin, Kincaidin) |> 
   summarise(
-    passrate = mean(pass, na.rm = T)
+    passrate = mean(pass, na.rm = T),
+    plays = n(),
+    PROE = mean(pass_oe, na.rm = T)
   )
-  
+
+
 pbp |> 
-  filter(posteam == 'DET', wp >= 0.025 & wp <= 0.975, down %in% c(1, 2)) |> 
+  filter(posteam == 'BUF', wp >= 0.025 & wp <= 0.975, down %in% c(1, 2),
+         yardline_100 >= 14) |> 
   group_by(play_id) |> 
   mutate(
-    LaPorta = any(full_name == 'Sam LaPorta')
-  ) |> 
-  group_by(LaPorta) |> 
+    Knoxin = any(full_name == 'Dawson Knox'),
+    Kincaidin = any(full_name == 'Dalton Kincaid')
+  ) |> slice(1) |> 
+  group_by(Knoxin, Kincaidin) |> 
   summarise(
-    passrate = mean(pass, na.rm = T)
-  )  
-  
+    passrate = mean(pass, na.rm = T),
+    plays = n(),
+    PROE = mean(pass_oe, na.rm = T)
+  )
 
 BUF_runpass <- pbp |> 
-  filter(posteam == 'BUF', wp >= 0.025 & wp <= 0.975, down %in% c(1, 2), position %in% c('HB', 'TE', 'FB', 'WR')) |> 
-  group_by(full_name, posteam) |> 
+  filter(posteam == 'BUF', wp >= 0.025 & wp <= 0.975, down %in% c(1, 2), 
+         position %in% c('HB', 'WR', 'TE',' FB')) |> 
+  group_by(full_name, position, posteam) |> 
   summarise(
     snapsplayed = n(),
     passrate = mean(pass, na.rm = T),
@@ -116,38 +118,28 @@ BUF_runpass <- pbp |>
   ) |> 
   left_join(teams_colors_logos |> select(team_abbr, team_wordmark), by = c('posteam' = 'team_abbr'))
 
-BUF_runpass |> 
-  select(full_name, team_wordmark, snapsplayed, passrate, rushrate, PROE) |> 
+BUFtbl <- BUF_runpass |> 
+  select(full_name, position, team_wordmark, snapsplayed, passrate, rushrate, PROE) |> 
   ungroup() |> 
   arrange(-snapsplayed) |> 
   gt() |> 
   gt_img_rows(team_wordmark) |> 
-  fmt_percent(c(passrate, rushrate, PROE), decimals = 2) |> 
-  tab_source_note("Table: @arjunmenon100 | Data: nflreadr") %>%
-  opt_align_table_header(align = "center") %>%
-  cols_align("center") %>%
+  fmt_percent(columns = c(passrate, rushrate, PROE), decimals = 2) |> 
+  opt_align_table_header("center") |> 
+  cols_align("center") |> 
+  tab_source_note("Table: Arjun Menon | Data: nflreadr") |> 
   cols_label(
-    full_name = 'Player',
-    team_wordmark = "",
+    full_name = "Player",
+    team_wordmark = "Offense",
     snapsplayed = "Snaps",
     passrate = "Pass Rate",
-    rushrate = 'Rush Rate',
-    PROE = 'Pass Rate Over Expected'
+    rushrate = "Rush Rate",
+    PROE = "Pass Rate Over Expected"
   ) |> 
-  opt_row_striping() %>%
-  tab_header(
-    title = md("Bills run pass rate by player in 2023")
-  ) %>% 
+  opt_row_striping() |> 
+  tab_header(title = "Bills run/pass rate by player in 2023") |> 
   gt_theme_538()
-gtsave(tbl, "weeklyperf.png")
-  
-  
-  
-
-
-
-
-
+gtsave(BUFtbl, "BUFrunpasstbl.png")
 
 
 
